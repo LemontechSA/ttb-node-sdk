bdca1db2cf1f791e93f68c3b1d46dbff6c0b5d51
Daniel Ochoa <dochoajohn@gmail.com>
2018-10-27 23:34:08 -0300

    Initial setup with rollup

---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import json from 'rollup-plugin-json'
+import { uglify } from 'rollup-plugin-uglify'
+import babel from 'rollup-plugin-babel'
+
+// `npm run build` -> `production` is true
+// `npm run dev` -> `production` is false
+const production = !process.env.ROLLUP_WATCH;
+
+export default {
+	input: 'src/index.js',
+	output: {
+		file: 'dist/ttb-node-sdk.js',
+		format: 'cjs',
+		sourcemap: true,
+	},
+	plugins: [
+		babel({
+      exclude: 'node_modules/**'
+    }),
+		json(),
+		resolve(),
+		commonjs(),
+		production && uglify()
+	]
+};
--- src/TimeBilling.js
+++ src/TimeBilling.js
@@ -1,4 +1,4 @@
-import V2 from './v2'
+import V2 from './v2/V2'
 
 export default {
   V2: (subdomain) => new V2(subdomain)
